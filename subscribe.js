b74465b48bd5a21ca3a76991ae1ae1dba1d0709c
pswoodworth <pswoodworth@gmail.com>
2017-05-13 18:44:12 -0400

    added hash

--- subscribe.js
+++ subscribe.js
@@ -3,61 +3,61 @@ const nomad = new Nomad()
 console.log(nomad);
 
 
-nomad.subscribe(['QmNgjvfuNJGyx2FEH6wgYpE9D9FwsSVjzyvKtoD7iZhT6z'], function(message) {
+nomad.subscribe(['QmNgjvfuNJGyx2FEH6wgYpE9D9FwsSVjzyvKtoD7iZhT6z', 'Qmb2zhhtBGHy7R1KhMpXpCBPTjYUCJWzEQKkjMFfcaPrDv'], function(message) {
   console.log('received new message');
   console.log(message.message);
 }, function(err){console.log(err)});
 
-// nomad.prepareToPublish().catch(function(err){
-//   console.log(err);
-// }).then(function(n) {
-//   console.log(n);
-//   const nomadInstance = n;
-//   // nomadInstance.publishRoot('root hi');
-//   var counter = 0;
-//   setInterval(function(){
-//     console.log('publishing');
-//     nomadInstance.publish(`hi ${counter}`);
-//     counter++;
-//   }, 2000);
-// });
+nomad.prepareToPublish().catch(function(err){
+  console.log(err);
+}).then(function(n) {
+  console.log(n);
+  const nomadInstance = n;
+  // nomadInstance.publishRoot('root hi');
+  var counter = 0;
+  setInterval(function(){
+    console.log('publishing');
+    nomadInstance.publish(`hi ${counter}`);
+    counter++;
+  }, 2000);
+});
 
 
-let idx = 0
-
-const createMessage = () => {
-  return `Message: ${idx++}`
-}
-
-let instance = null
-nomad.prepareToPublish()
-  .then((n) => {
-    instance = n
-    console.log('DEMO: CONNECTED!!!!')
-    return instance.publishRoot('ROOT MESSAGE')
-  })
-  .catch(() => {
-    log.err('Error publishing root message')
-  })
-  .then(() => {
-    console.log('READY')
-    console.log('DEMO: ROOT PUBLISHED!!!!')
-    setInterval(() => {
-	const msg = createMessage();
-	console.log('sending message', msg);
-      instance.publish(msg)
-    }, 60000)
-    return nomad.publish(createMessage())
-  })
-  .catch((err) => {
-    log.err('err')
-    console.log('DEMO: CONNECT ERROR!!!!', e)
-    console.log(e)
-    return nomad.publish('Hey there, Gavin!')
-  })
-  .then(() => {
-    console.log('DEMO: PUBLLISHED!!!!', d)
-  })
-  .catch(() => {
-    console.log('DEMO: PUBLISH ERROR!!!!', e)
-  })
+// let idx = 0
+// 
+// const createMessage = () => {
+//   return `Message: ${idx++}`
+// }
+// 
+// let instance = null
+// nomad.prepareToPublish()
+//   .then((n) => {
+//     instance = n
+//     console.log('DEMO: CONNECTED!!!!')
+//     return instance.publishRoot('ROOT MESSAGE')
+//   })
+//   .catch(() => {
+//     log.err('Error publishing root message')
+//   })
+//   .then(() => {
+//     console.log('READY')
+//     console.log('DEMO: ROOT PUBLISHED!!!!')
+//     setInterval(() => {
+// 	const msg = createMessage();
+// 	console.log('sending message', msg);
+//       instance.publish(msg)
+//     }, 60000)
+//     return nomad.publish(createMessage())
+//   })
+//   .catch((err) => {
+//     log.err('err')
+//     console.log('DEMO: CONNECT ERROR!!!!', e)
+//     console.log(e)
+//     return nomad.publish('Hey there, Gavin!')
+//   })
+//   .then(() => {
+//     console.log('DEMO: PUBLLISHED!!!!', d)
+//   })
+//   .catch(() => {
+//     console.log('DEMO: PUBLISH ERROR!!!!', e)
+//   })
